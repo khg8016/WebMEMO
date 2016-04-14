a824ac1a413b2f268fa94708e5053293d5539783
khg8016 <khg8016@nate.com>
2016-04-14 19:43:13 +0900

    111

--- public/memo/controllers/client.memo.memoModalCtrl.js
+++ public/memo/controllers/client.memo.memoModalCtrl.js
@@ -22,58 +22,58 @@ angular.module('memo').controller('memoModalController', ['$scope', '$location',
                     $scope.commentToggle[i] = true;
                 }
             }
-        };
+                            };
 
-            $scope.close = function(result) {
-                close(result, 100);
-                $location.path('/main/' + $routeParams.boardId + '/memo');
-            };
+                            $scope.close = function(result) {
+                                close(result, 100);
+                                $location.path('/main/' + $routeParams.boardId + '/memo');
+                            };
 
-            /* $scope.delete = function(){
+                            /* $scope.delete = function(){
 
-             $scope.memo.$remove({boardId: $routeParams.boardId},
-             function (){
-             close(100);
-             $location.path('/main/' + $routeParams.boardId + '/memo');
-             });
+                             $scope.memo.$remove({boardId: $routeParams.boardId},
+                             function (){
+                             close(100);
+                             $location.path('/main/' + $routeParams.boardId + '/memo');
+                             });
 
-             };
-             */
-            $scope.update = function(){
-                $scope.memo.$update({boardId: $routeParams.boardId},
-                    function(response){
-                        $scope.memoToggle = true;
-                    }, function(errorResponse){
-                        $scope.error = errorResponse.data.message;
-                    }
-                );
-            };
+                             };
+                             */
+                            $scope.update = function(){
+                                $scope.memo.$update({boardId: $routeParams.boardId},
+                                    function(response){
+                                        $scope.memoToggle = true;
+                                    }, function(errorResponse){
+                                        $scope.error = errorResponse.data.message;
+                                    }
+                                );
+                            };
 
-            $scope.makeComment = function(){
-            var comment = new Comments({
-               content : this.comment
-            });
+                            $scope.makeComment = function(){
+                                var comment = new Comments({
+                                    content : this.comment
+                                });
 
-            comment.$save({boardId: $routeParams.boardId, memoId: $routeParams.memoId},
-                function(comment){
-                    console.log("fdsafs" + comment._id);
-                    $scope.comment = "";
-                    $scope.comments.push(comment);
+                                comment.$save({boardId: $routeParams.boardId, memoId: $routeParams.memoId},
+                                    function(comment){
+                                        console.log("fdsafs" + comment._id);
+                                        $scope.comment = "";
+                                        $scope.comments.push(comment);
 
-                    $scope.commentToggle[$scope.comments.length] = false;
-                }, function(){
-                    console.log("Error");
-                });
-        };
+                                        $scope.commentToggle[$scope.comments.length] = false;
+                                    }, function(){
+                                        console.log("Error");
+                                    });
+                            };
 
-        $scope.deleteComment = function(comment){
-            if(comment) {
-                console.log($scope.comments[0] == comment);
-                comment.$remove({boardId: $routeParams.boardId, memoId: $routeParams.memoId, commentId : comment._id},
-                    function () {
-                        console.log("success");
-                        for(var i in $scope.comments){
-                            if($scope.comments[i] === comment){
+                            $scope.deleteComment = function(comment){
+                                if(comment) {
+                                    console.log($scope.comments[0] == comment);
+                                    comment.$remove({boardId: $routeParams.boardId, memoId: $routeParams.memoId, commentId : comment._id},
+                                        function () {
+                                            console.log("success");
+                                            for(var i in $scope.comments){
+                                                if($scope.comments[i] === comment){
                                 $scope.comments.splice(i, 1);
                                 $scope.commentToggle.splice(i, 1);
                             }
